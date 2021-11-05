fd0ebcc090c14ef7cb998e387a3f4cb16007197f
Sofi <w0lfskitten173@gmail.com>
2021-11-05 23:16:22 +1100

    commitMessage!

--- Git Easy Push.bat
+++ Git Easy Push.bat
@@ -6,12 +6,7 @@ git add .
 
 git status
 
-:invalidCommitMessage
 set /P commitMessage = "Enter commit message here: "
-
-set invalidCommit = ""
-
-if commitMessage == "%invalidCommit%" echo Commit Message Can't Be Empty
 
 git commit -m commitMessage%!
 
